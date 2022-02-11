24db9284a076aebeb50b314366f2608ef9dbb97e
Marek <51706013+technicalflow@users.noreply.github.com>
2022-02-11 09:05:12 +0100

    small changes

--- bash/unifi.sh
+++ bash/unifi.sh
@@ -3,18 +3,20 @@
 apt-get update && apt-get upgrade -y
 apt-get install ca-certificates apt-transport-https nano htop wget gnupg
 echo 'deb https://www.ui.com/downloads/unifi/debian stable ubiquiti' | tee /etc/apt/sources.list.d/100-ubnt-unifi.list
-wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ui.com/unifi/unifi-repo.gpg
-apt-key adv --keyserver keyserver.ubuntu.com --recv 06E85760C0A52C50
+# apt-key adv --keyserver keyserver.ubuntu.com --recv 06E85760C0A52C50
 wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ui.com/unifi/unifi-repo.gpg
 apt-get update && apt-get install unifi -y
 
 systemctl status unifi
 nano /usr/lib/unifi/data/system.properties
+nano /etc/init.d/unifi
 
 systemctl restart unifi
 systemctl status unifi
 java version
 
+#Logs
 cat /var/log/unifi/server.log
-nano /etc/init.d/unifi
-nano /usr/lib/unifi/data/system.properties
+
+# IP:8080
+# IP:8443
