ac075ae1eeeaa094d2d20e84e0fb878bebc474b6
Marek Serba <marekserba@microsoft.com>
2022-02-12 17:20:53 +0100

    small updates

--- bash/plex.sh
+++ bash/plex.sh
@@ -6,7 +6,7 @@
 
 apt update && apt upgrade -y
 apt install -y nano htop curl gnupg ufw
-echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
+echo deb https://downloads.plex.tv/repo/deb public main | tee /etc/apt/sources.list.d/plexmediaserver.list
 curl https://downloads.plex.tv/plex-keys/PlexSign.key | apt-key add -
 # curl https://downloads.plex.tv/plex-keys/PlexSign.key > plexsign.key
 # apt-key add plexsign.key
@@ -32,7 +32,6 @@ description=The Plex Media Server (with additional DLNA capability)
 ports=32400/tcp|3005/tcp|5353/udp|8324/tcp|32410:32414/udp|1900/udp|32469/tcp
 EOFplex
 
-service --status-all
 systemctl status plex*
 
 ufw status
@@ -43,3 +42,5 @@ ufw allow plexmediaserver
 sudo ufw status verbose
 ufw enable
 ufw status verbose
+
+# go to serverip:32400/web
