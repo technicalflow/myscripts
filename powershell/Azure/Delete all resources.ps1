acece7efe02dddf46a60c416e70818521131d96f
Marek <51706013+technicalflow@users.noreply.github.com>
2022-11-06 14:13:49 +0100

    cleanup

--- powershell/Azure/Delete all resources.ps1
+++
@@ -1,6 +0,0 @@
-$RG=rgname
-do {
-    foreach ($a in $(Get-AzResource -ResourceGroupName $RG).Id) 
-    {Remove-AzResource -ResourceId $a -Force}
-    } 
-Until ((Get-AzResource -ResourceGroupName $RG).Id -eq $null)
--- powershell/Curl tls 1.2.ps1
+++
@@ -1,2 +0,0 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
-[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
